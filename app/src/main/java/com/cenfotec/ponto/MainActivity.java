2450c6bde04358e21bf0cc0959712d9b9cb0a66c
Ernesto Desanti <netodesanti@hotmail.com>
2020-03-17 17:51:55 -0600

    Estilos formulario

--- app/src/main/java/com/cenfotec/ponto/MainActivity.java
+++ app/src/main/java/com/cenfotec/ponto/MainActivity.java
@@ -8,10 +8,12 @@ import android.view.View;
 import android.widget.Button;
 
 import com.cenfotec.ponto.entities.bidder.BidderRegistrationActivity;
+import com.cenfotec.ponto.entities.petitioner.PetitionerRegistrationActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnSwapToBidderRegistration;
+    Button btnPetitioner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +27,23 @@ public class MainActivity extends AppCompatActivity {
                 openBidderRegistrationView();
             }
         });
+
+        btnPetitioner = findViewById(R.id.btnSolicitante);
+        btnPetitioner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    openPetitionerRegistrationView();
+            }
+        });
     }
 
     private void openBidderRegistrationView(){
         Intent intent = new Intent(this, BidderRegistrationActivity.class);
         startActivity(intent);
+    }
+
+    private void openPetitionerRegistrationView(){
+        Intent i = new Intent(this, PetitionerRegistrationActivity.class);
+        this.startActivity(i);
     }
 }
